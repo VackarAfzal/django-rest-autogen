3ff7db6691031196127d8225a05335b4eeb66c3a
vackarafzal <v.z.afzal@dundee.ac.uk>
2017-12-26 21:48:24 +0000

    Allow for configurable viewset base, serialiser base and permissions

--- django_rest_autogen/core.py
+++ django_rest_autogen/core.py
@@ -5,21 +5,21 @@ from django.apps import apps
 
 class AutoGenRouter(object):
 
-    def get_default_router(self):
-        '''
-        Gets a default router pre-configured for all models
-        :return: The default router pre-configured for all models
-        '''
+    def _get_router(self, viewset_cls=None, serialiser_cls=None, permissions=None):
         autogen_router = routers.DefaultRouter()
 
-        for mdl in  apps.get_models():
+        viewset_cls     = viewset_cls or viewsets.ModelViewSet
+        serialiser_cls  = serialiser_cls or serializers.ModelSerializer
+        permissions     = permissions or (IsAdminUser,)
+
+        for mdl in apps.get_models():
             model = mdl
             resource_name = model.__name__
 
-            ModelViewSet = type('{0}ViewSet'.format(resource_name), (viewsets.ModelViewSet,), {
+            cls_config = type('{0}ViewSet'.format(resource_name), (viewset_cls,), {
                 'queryset': model.objects.all(),
-                'permission_classes': (IsAdminUser,),
-                'serializer_class': type('{0}Serialiser'.format(resource_name), (serializers.ModelSerializer,), {
+                'permission_classes': permissions,
+                'serializer_class': type('{0}Serialiser'.format(resource_name), (serialiser_cls,), {
                     'Meta': type('Meta', (object,), {
                         'model': model,
                         'fields': '__all__'
@@ -27,7 +27,15 @@ class AutoGenRouter(object):
                 })
             })
 
-            autogen_router.register(resource_name, ModelViewSet)
+            autogen_router.register(resource_name, cls_config)
 
         return autogen_router
+
+
+    def get_default_router(self):
+        '''
+        Gets a default router pre-configured for all models
+        :return: The default router pre-configured for all models
+        '''
+        return self._get_router()
 
