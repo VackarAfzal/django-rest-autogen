e71e84896a64a387d55ca9e3facf94b6af2cf7b1
vackarafzal <v.z.afzal@dundee.ac.uk>
2017-12-26 21:48:43 +0000

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-rest-autogen',
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/VackarAfzal/django-rest-autogen',
     author='Vackar Afzal',
     author_email='v.z.afzal@dundee.ac.uk',
