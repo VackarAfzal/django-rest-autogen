fb70dac7769c1c22e92b6d2518764355008d2d48
vackarafzal <v.z.afzal@dundee.ac.uk>
2017-12-26 22:28:17 +0000

    Add in options for proper filtering

--- django_rest_autogen/core.py
+++ django_rest_autogen/core.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets, serializers, routers
 from rest_framework.permissions import IsAdminUser
 from django.apps import apps
+import rest_framework_filters as filters
+
 
 
 class AutoGenRouter(object):
 
-    def _get_router(self, viewset_cls=None, serialiser_cls=None, permissions=None):
+    def _get_router(self, viewset_cls=None, serialiser_cls=None, permissions=None, filter_cls=None):
         autogen_router = routers.DefaultRouter()
 
         viewset_cls     = viewset_cls or viewsets.ModelViewSet
@@ -16,7 +18,7 @@ class AutoGenRouter(object):
             model = mdl
             resource_name = model.__name__
 
-            cls_config = type('{0}ViewSet'.format(resource_name), (viewset_cls,), {
+            cls_props = {
                 'queryset': model.objects.all(),
                 'permission_classes': permissions,
                 'serializer_class': type('{0}Serialiser'.format(resource_name), (serialiser_cls,), {
@@ -25,7 +27,12 @@ class AutoGenRouter(object):
                         'fields': '__all__'
                     })
                 })
-            })
+            }
+
+            if filter_cls:
+                cls_props['filter_class'] = filter_cls
+
+            cls_config = type('{0}ViewSet'.format(resource_name), (viewset_cls,), cls_props)
 
             autogen_router.register(resource_name, cls_config)
 
@@ -37,5 +44,5 @@ class AutoGenRouter(object):
         Gets a default router pre-configured for all models
         :return: The default router pre-configured for all models
         '''
-        return self._get_router()
+        return self._get_router(filter_cls=filters.AllLookupsFilter)
 
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name='django-rest-autogen',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/VackarAfzal/django-rest-autogen',
     author='Vackar Afzal',
     author_email='v.z.afzal@dundee.ac.uk',
     description=('A libarry to auto-gen rest-framework endpoint for all your django models.'),
     license='MIT',
-    install_requires=['djangorestframework >= 3.6.3'],
+    install_requires=['djangorestframework >= 3.6.3', 'djangorestframework-filters==0.10.2'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
